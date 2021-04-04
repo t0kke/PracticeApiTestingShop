d684838c1189801f44f239dd3ece7b6134c3f2c1
German <k4tger@gmail.com>
2021-04-04 22:28:52 +0300

    Added configure driver method

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,7 +2,13 @@ package tests;
 
 import api.steps.ApiSteps;
 import api.steps.UISteps;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static config.ConfigHelper.isVideoOn;
+import static helpers.AttachmentsHelper.*;
+import static helpers.DriverHelpers.*;
 
 public class BaseTest {
     static ApiSteps restAssured;
@@ -12,5 +18,18 @@ public class BaseTest {
     public static void setUp() {
         restAssured = new ApiSteps();
         ui = new UISteps();
+        configureDriver();
+    }
+
+    @AfterEach
+    public void addAttachments() {
+        String sessionId = getSessionId();
+
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if (isVideoOn()) attachVideo(sessionId);
+
+        closeWebDriver();
     }
 }
