195a443c84861e5d87122acd49bac35139102fd4
German <k4tger@gmail.com>
2021-04-05 10:02:31 +0300

    Added Listener for api tests

--- src/test/java/tests/ApiBaseTest.java
+++ src/test/java/tests/ApiBaseTest.java
@@ -2,7 +2,10 @@ package tests;
 
 import api.steps.ApiSteps;
 import api.steps.UISteps;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class ApiBaseTest {
 	static ApiSteps restAssured;
@@ -12,5 +15,6 @@ public class ApiBaseTest {
 	public static void setUp() {
 		restAssured = new ApiSteps();
 		ui = new UISteps();
+		addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
 	}
 }
