269b9786a4cec6c2d6567db7ec75f5dd8a546942
t0kke <k4tger@gmail.com>
2021-05-06 21:59:34 +0300

    Added annotation jira issue for test

--- src/test/java/tests/UIShopTests.java
+++ src/test/java/tests/UIShopTests.java
@@ -1,9 +1,11 @@
 package tests;
 
+import annotations.JiraIssue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UIShopTests extends UIBaseTest {
+    @JiraIssue("QC3-32")
     @Test
     @DisplayName("Проверка добавления товара в список избранного через UI")
     void checkProductAdditionToWishListPopUpInUI() {
